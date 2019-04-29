0f874e8e54052a391bb0402c65ffcdbb428cb162
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2019-04-29 11:05:50 +0200

    Implement DepartmentViewModel GetAvailability

--- Projekt/NoNameBooking/UnitTestProject/DepartmentViewModelUnitTest.cs
+++ Projekt/NoNameBooking/UnitTestProject/DepartmentViewModelUnitTest.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModelClassLibrary;
 using ViewModelClassLibrary;
 
 namespace UnitTestProject
@@ -17,7 +18,8 @@ namespace UnitTestProject
         [TestInitialize]
         public void DepartmentViewModelTestSetup()
         {
-            _testDepartmentViewModel = new DepartmentViewModel();
+            Department testDepartment = new Department("Johannes", "Testvej 12");
+            _testDepartmentViewModel = new DepartmentViewModel(testDepartment);
         }
 
         [TestMethod]
@@ -29,7 +31,28 @@ namespace UnitTestProject
         [TestMethod]
         public void GetAvailabilityTest()
         {
+            User testUser = new User();
+            Practitioner testPractitioner = new Practitioner(new DateTime(1, 1, 1, 9, 0, 0), TimeSpan.FromHours(12));
+            List<User> users = new List<User>();
 
+            DateTime testDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.AddDays(3).Day,
+                12, 0, 0);
+
+            AppointmentType testAppointmentType = new AppointmentType("Sebastian", 995.12, TimeSpan.FromHours(2));
+
+            Room testRoom = new Room("Fisk");
+
+            users.Add(testUser);
+            users.Add(testPractitioner);
+
+            Appointment testAppointment = new Appointment(testDate, users, testAppointmentType, testRoom, "");
+
+            DateTime startDate = DateTime.Today.AddDays(1);
+            DateTime endDate = DateTime.Today.AddMonths(1);
+
+            List<DateTime> availableDateTimes = _testDepartmentViewModel.GetAvailability(startDate, endDate, testPractitioner);
+
+            Assert.IsFalse(availableDateTimes.Contains(testAppointment.DateAndTime));
         }
     }
 }
--- Projekt/NoNameBooking/ViewModelClassLibrary/DepartmentViewModel.cs
+++ Projekt/NoNameBooking/ViewModelClassLibrary/DepartmentViewModel.cs
@@ -1,7 +1,41 @@
-﻿namespace ViewModelClassLibrary
+﻿using System;
+using System.Collections.Generic;
+using ModelClassLibrary;
+
+namespace ViewModelClassLibrary
 {
     public class DepartmentViewModel
     {
-        
+
+        private Department _department;
+
+        public DepartmentViewModel(Department department)
+        {
+            _department = department;
+        }
+        public List<DateTime> GetAvailability(DateTime startDate, DateTime endDate, Practitioner practitioner)
+        {
+            List<DateTime> departmentAvailableDateTimes = _department.GetAvailability(startDate, endDate);
+
+            List<DateTime> practitionerAvailableDateTimes = practitioner.GetAvailability(startDate, endDate);
+
+            return CompareAvailability(departmentAvailableDateTimes, practitionerAvailableDateTimes);
+
+        }
+
+        private List<DateTime> CompareAvailability(List<DateTime> departmentAvailableDateTimes, List<DateTime> practitionerAvailableDateTimes)
+        {
+            List<DateTime> combinedAvailableDateTimes = new List<DateTime>();
+
+            foreach (DateTime departmentAvailableDateTime in departmentAvailableDateTimes)
+            {
+                if (!practitionerAvailableDateTimes.Contains(departmentAvailableDateTime))
+                {
+                    combinedAvailableDateTimes.Add(departmentAvailableDateTime);
+                }
+            }
+
+            return combinedAvailableDateTimes;
+        }
     }
 }
