1e74a99647c75b95e4bb2e1af407e510de169cb7
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2019-04-29 10:24:30 +0200

    Add DepartmentViewModelUnitTest and DepartmentViewModel

---
+++ Projekt/NoNameBooking/UnitTestProject/DepartmentViewModelUnitTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViewModelClassLibrary;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    /// Summary description for DepartmentViewModelUnitTest
+    /// </summary>
+    [TestClass]
+    public class DepartmentViewModelUnitTest
+    {
+        private DepartmentViewModel _testDepartmentViewModel;
+
+        [TestInitialize]
+        public void DepartmentViewModelTestSetup()
+        {
+            _testDepartmentViewModel = new DepartmentViewModel();
+        }
+
+        [TestMethod]
+        public void CreationUnitTest()
+        {
+            Assert.IsNotNull(_testDepartmentViewModel);
+        }
+    }
+}
