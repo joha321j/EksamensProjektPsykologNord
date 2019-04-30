ac1b7c01b008d8fac08f57892686ca6f1a7a8810
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2019-04-30 14:04:18 +0200

    Updated the Updated StoredProcedures with Update.

--- StoredProcedures Setup.sql
+++ StoredProcedures Setup.sql
@@ -38,7 +38,7 @@ BEGIN
 END
 GO
 
-CREATE PROCEDURE SPUpdateDepartMent
+CREATE PROCEDURE SPUpdateDepartment
 @DepartmentId int,
 @Address nvarchar(max),
 @Name nvarchar(max)
@@ -92,3 +92,140 @@ BEGIN
 END
 GO
 
+CREATE PROCEDURE SPUpdateRoom
+@RoomId int,
+@DepartmentId int,
+@Name nvarchar(max)
+
+AS
+BEGIN
+	UPDATE PN_ROOM
+	SET		DepartmentId = @DepartmentId,
+			Name = @Name
+	WHERE	Id = @RoomId
+END
+GO
+
+CREATE PROCEDURE SPUpdateTreatmentType
+@TreatmentTypeId int,
+@Name nvarchar(max),
+@Duration datetime2(7),
+@StandardPrice float
+
+AS
+BEGIN
+	UPDATE	PN_TreatmentType
+	SET		Name = @Name,
+			Duration = @Duration,
+			StandardPrice = @StandardPrice
+	WHERE	Id = @TreatmentTypeId
+END
+GO
+
+CREATE PROCEDURE SPUdateUser
+@UserId int,
+@Name nvarchar(max),
+@Address nvarchar(max),
+@PhoneNumber nvarchar(12),
+@Email nvarchar(max)
+
+AS
+BEGIN
+	UPDATE PN_User
+	SET		name = @Name,
+			Address = @Address,
+			PhoneNumber = @PhoneNumber,
+			Email = @Email
+	WHERE	Id = @UserId
+END
+GO
+
+CREATE PROCEDURE SPDeleteAppointment
+@AppointmentId int
+
+AS
+BEGIN
+	DELETE from PN_Appointment
+	WHERE	Id = @AppointmentId
+END
+GO
+
+CREATE PROCEDURE SPDeleteClient
+@ClientId int
+
+AS
+BEGIN
+	DELETE from PN_Client
+	WHERE	Id = @ClientId
+END
+GO
+
+CREATE PROCEDURE SPDeleteDepartment
+@DepartmentId int
+
+AS
+BEGIN
+	DELETE from PN_Department
+	WHERE	Id = @DepartmentId
+END
+GO
+
+CREATE PROCEDURE SPDeleteInvoice
+@InvoiceId int
+
+AS
+BEGIN
+	DELETE from PN_Invoice
+	WHERE Id = @InvoiceId
+END
+GO
+
+CREATE PROCEDURE SPDeleteJournal_Entry
+@Journal_EntryId int
+
+AS
+BEGIN
+	DELETE from PN_Journal_Entry
+	WHERE Id = @Journal_EntryId
+END
+GO
+
+CREATE PROCEDURE SPDeletePractitioner
+@PractitionerId int
+
+AS
+BEGIN
+	DELETE from PN_Practitioner
+	WHERE Id = @PractitionerId
+END
+GO
+
+CREATE PROCEDURE SPDeleteRoom
+@RoomId int
+
+AS
+BEGIN
+	DELETE from PN_Room
+	WHERE id = @RoomId
+END
+GO
+
+CREATE PROCEDURE SPDeleteTreatmentType
+@TreatmentTypeId int
+
+AS
+BEGIN
+	DELETE from PN_TreatmentType
+	WHERE Id = @TreatmentTypeId
+END
+GO
+
+CREATE PROCEDURE SPDeleteUser
+@UserId int
+
+AS
+BEGIN
+	DELETE from PN_User
+	WHERE Id = @UserId
+END
+GO
