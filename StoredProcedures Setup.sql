dc722dbcdd8afd67f172a3f698a749ef3ebd32a4
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2019-05-01 10:40:15 +0200

    create query og select
    Lavet ud fra CRUD.

--- StoredProcedures Setup.sql
+++ StoredProcedures Setup.sql
@@ -1,3 +1,219 @@
+CREATE PROCEDURE SPInsertAppointment
+@AppointmentId int,
+@DateAndTime datetime2(7),
+@RoomId int,
+@PractitionerId int,
+@Price float,
+@TreatmentTypeId int,
+@Note nvarchar(max)
+
+AS
+BEGIN
+
+	INSERT INTO PN_Appointment(Id, DateAndTime, RoomId, PractitionerId, Price, TreatmentTypeId, Note)
+			VALUES(@AppointmentId, @DateAndTime, @RoomId, @PractitionerId, @Price, @TreatmentTypeId, @Note)
+
+END
+GO
+
+CREATE PROCEDURE SPInsertClient
+@ClientId int,
+@MedicalRefferal bit,
+@JournalId int,
+@SocialSecurityNumber int,
+@UserId int
+
+AS
+BEGIN
+	INSERT INTO PN_Client(Id, MedicalReferral, Journalid, SocialSecurityNumber, UserId)
+			VALUES(@ClientId, @MedicalRefferal, @JournalId, @SocialSecurityNumber, @UserId)
+END
+GO
+
+CREATE PROCEDURE SPInsertDepartment
+@DepartmentId int,
+@Address nvarchar(max),
+@Name nvarchar(max)
+
+AS
+BEGIN
+	INSERT INTO PN_Department(Id, Address, Name)
+			VALUES(@DepartmentId, @Address, @Name)
+END
+GO
+
+CREATE PROCEDURE SPInsertInvoice
+@InvoiceId int,
+@DueDate datetime2(7),
+@AppointmentId int
+
+AS
+BEGIN
+	INSERT INTO PN_Invoice(Id, DueDate, AppointmentId)
+			VALUES(@InvoiceId, @DueDate, @AppointmentId)
+END
+GO
+
+CREATE PROCEDURE SPInsertUser
+@UserId int,
+@Name nvarchar(max),
+@Address nvarchar(max),
+@PhoneNumber nvarchar(12),
+@Email nvarchar(max)
+
+AS
+BEGIN
+	INSERT INTO PN_User(Id, Name, Address, PhoneNumber, Email)
+			VALUES(@UserId, @Name, @Address, @PhoneNumber, @Email)
+END
+GO
+
+CREATE PROCEDURE SPInsertJournal_Entry
+@Journal_EntryId int,
+@JournalId int,
+@Text nvarchar(max)
+
+AS
+BEGIN
+	INSERT INTO PN_Journal_Entry(Id,JournalId,Text)
+			VALUES(@Journal_EntryId, @JournalId, @Text)
+END
+GO
+
+CREATE PROCEDURE SPInsertPractitioner
+@PractitonerId int,
+@UserId int
+
+AS
+BEGIN
+	INSERT INTO PN_Practitioner(Id, UserId)
+			VALUES(@PractitonerId, @UserId)
+END
+GO
+
+CREATE PROCEDURE SPInsertRoom
+@RoomId int,
+@DepartmentId int,
+@Name nvarchar(max)
+
+AS
+BEGIN
+	INSERT INTO PN_Room(Id,DepartmentId,Name)
+			VALUES(@RoomId,@DepartmentId,@Name)
+END
+GO
+
+CREATE PROCEDURE SPInsertTreatmentType
+@TreatmentTypeId int,
+@Name nvarchar(max),
+@Duration datetime2(7),
+@StandardPrice float
+
+AS
+BEGIN
+	INSERT INTO PN_TreatmentType(Id, Name, Duration, StandardPrice)
+			VALUES(@TreatmentTypeId, @Name, @Duration, @StandardPrice)
+END
+GO
+
+CREATE PROCEDURE SPSelectAppointment
+@AppointmentId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_APPOINTMENT
+	WHERE Id like '%'+@AppointmentId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectClient
+@ClientId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Client
+	WHERE Id like '%'+@ClientId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectDepartment
+@DepartmentId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Department
+	WHERE Id like '%'+@DepartmentId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectInvoice
+@InvoiceId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Invoice
+	WHERE Id like '%'+@InvoiceId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectJournal_Entry
+@Journal_EntryId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Journal_Entry
+	WHERE Id like '%'+@Journal_EntryId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectPractitioner
+@PractitionerId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Practitioner
+	WHERE Id like '%'+@PractitionerId+'%'
+END
+GO
+CREATE PROCEDURE SPSelectRoom
+@RoomId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_Room
+	WHERE Id like '%'+@RoomId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectTreatmentType
+@TreatmentTypeId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_TreatmentType
+	WHERE Id like '%'+@TreatmentTypeId+'%'
+END
+GO
+
+CREATE PROCEDURE SPSelectUser
+@UserId int
+
+AS
+BEGIN
+
+	SELECT * FROM PN_User
+	WHERE Id like '%'+@UserId+'%'
+END
+GO
+
 CREATE PROCEDURE SPUpdateAppointment
 @AppointmentId int,
 @DateAndTime datetime2(7),
