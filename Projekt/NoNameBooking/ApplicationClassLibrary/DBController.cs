d024c6d9eb9a70c4549e8f173064a180ea16a002
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2019-05-01 12:32:05 +0200

    Oprettede klassen

--- Projekt/NoNameBooking/ApplicationClassLibrary/DBController.cs
+++ Projekt/NoNameBooking/ApplicationClassLibrary/DBController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace ApplicationClassLibrary
 {
@@ -8,5 +10,30 @@ namespace ApplicationClassLibrary
     {
         private static string _connectionString = "Server=EALSQL1.eal.local; Database = B_DB19_2018; User Id = B_STUDENT19; Password = B_OPENDB19; MultipleActiveResultSets=True;";
 
+        public void AddAppointment(int id, string connectionString, DateTime dateAndTime, string note)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand("spInsertAppointment", connection);
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.Parameters.AddWithValue("@DateAndTime", dateAndTime);
+                    command.Parameters.AddWithValue("@")
+                    command.Parameters.AddWithValue("@Note", note);
+
+                    command.ExecuteNonQuery();
+                    command.Parameters.Clear();
+
+                }
+            }
+            catch (Exception e)
+            {
+                /// TODO: Actually handle the exception!
+                throw e;
+            }
+        }
     }
 }
