81fb9c76f18598cf0c9b6344dd1a7abf1a058e81
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2019-05-01 11:55:01 +0200

    hahaha thanos car aint real he cant hurt you

---
+++ Projekt/NoNameBooking/ApplicationClassLibrary/DBController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationClassLibrary
+{
+    class DBController
+    {
+        private static string _connectionString = "Server=EALSQL1.eal.local; Database = B_DB19_2018; User Id = B_STUDENT19; Password = B_OPENDB19; MultipleActiveResultSets=True;";
+
+    }
+}
