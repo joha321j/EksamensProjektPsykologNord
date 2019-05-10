0213c4069e2ec9e7ea9c238888b485fc9600982e
ande714b <43536499+ande714b@users.noreply.github.com>
2019-05-10 12:22:01 +0200

    renaming

--- Projekt/NoNameBooking/ModelClassLibrary/Client.cs
+++ Projekt/NoNameBooking/ModelClassLibrary/Client.cs
@@ -4,16 +4,16 @@ namespace ModelClassLibrary
 {
     public class Client : User
     {
-        internal string ClientSsn { get; }
-        public string ClientNote { get; }
+        internal string SocialSecurityNumber { get; }
+        public string Note { get; }
         public Journal Journal { get; }
 
         public Client(string clientName = "", string clientEmail = "", string clientPhoneNumber = "",
-            string clientAddress = "", string clientSsn = "-1", string clientNote = "", int id = -1)
+            string clientAddress = "", string socialSecurityNumber = "-1", string note = "", int id = -1)
             : base(clientName, clientAddress, clientPhoneNumber, clientEmail, id)
         {
-            ClientSsn = clientSsn;
-            ClientNote = clientNote;
+            SocialSecurityNumber = socialSecurityNumber;
+            Note = note;
             Journal = new Journal();
         }
     }
