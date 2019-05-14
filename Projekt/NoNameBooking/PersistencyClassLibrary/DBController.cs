a9c4fe4c6c1622e13edf7e5546a488808b1d27ab
ande714b <43536499+ande714b@users.noreply.github.com>
2019-05-14 08:22:20 +0200

    reference

--- Projekt/NoNameBooking/PersistencyClassLibrary/DBController.cs
+++ Projekt/NoNameBooking/PersistencyClassLibrary/DBController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Globalization;
-using ApplicationClassLibrary;
 using ModelClassLibrary;
 
 namespace PersistencyClassLibrary
