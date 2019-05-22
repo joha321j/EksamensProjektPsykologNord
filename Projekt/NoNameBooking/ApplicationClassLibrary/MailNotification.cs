92481924b49956c940cf8c09e967020fbca43bea
Sebastian Thorup Frederiksen <sthorupf@hotmail.com>
2019-05-22 09:49:24 +0200

    new mails for different things

--- Projekt/NoNameBooking/ApplicationClassLibrary/MailNotification.cs
+++ Projekt/NoNameBooking/ApplicationClassLibrary/MailNotification.cs
@@ -10,6 +10,7 @@ namespace ApplicationClassLibrary
 {
     class MailNotification
     {
+        private readonly ClientRepo _clientRepo;
         public void SendTestMail(User user)
         {
             MimeMessage message = new MimeMessage();
@@ -42,17 +43,132 @@ namespace ApplicationClassLibrary
 
         public void AppointmentCreatedEmail(Appointment appointment)
         {
-            //Send email to all involved users that an appointment has been made and at what time.
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Kaare Veggerby Sandbøl", "kaar1498@edu.eal.dk"));
+            User user = new User();
+            foreach (User tempUser in appointment.Participants)
+            {
+                if (_clientRepo.IsClient(tempUser))
+                {
+                    user = tempUser;
+                }
+                
+            }
+            message.To.Add(new MailboxAddress(user.Name, user.Email));
+
+            message.Subject = "Bekræftelses Email til " + user.Name;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Dette er en bekræftigelse på at " + user.Name + "'s aftale d. "+ appointment.DateAndTime.ToString("dd/MM/yyyy HH:mm")+"." +
+                "\n Vi glæder os til at se dig" +
+                "\n Venlig hilsen" +
+                "\n Semplito"
+
+            };
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.sendgrid.net", 587, false);
+
+                client.Authenticate("apikey", "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");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
         public void AppointmentUpdatedEmail(Appointment appointment)
         {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Kaare Veggerby Sandbøl", "kaar1498@edu.eal.dk"));
+            User user = new User();
+            User prac = new User();
+            foreach (User tempUser in appointment.Participants)
+            {
+                if (_clientRepo.IsClient(tempUser))
+                {
+                    user = tempUser;
+                }
+                else
+                {
+                    prac = tempUser;
+                }
 
+            }
+            message.To.Add(new MailboxAddress(user.Name, user.Email));
+
+            message.Subject = "Bekræftelses Email til " + user.Name;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Dette er en meddelelse om at " + user.Name + "'s aftale med "+ prac.Name +" er blevet rygget." +
+                "\nDin tid er nu d. "+ appointment.DateAndTime.ToString("dd/MM/yyyy HH:mm")+"."+
+                "\n Vi glæder os til at se dig" +
+                "\nHvis der er nogle problemer eller spørgsmål til din tid er du som altid velkommen til at kontakte os på mobil eller mail"+
+                "\n Venlig hilsen" +
+                "\n Semplito"
+
+            };
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.sendgrid.net", 587, false);
+
+                client.Authenticate("apikey", "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");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
         public void AppointmentDeletedEmail(Appointment appointment)
         {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Kaare Veggerby Sandbøl", "kaar1498@edu.eal.dk"));
+            User user = new User();
+            User prac = new User();
+            foreach (User tempUser in appointment.Participants)
+            {
+                if (_clientRepo.IsClient(tempUser))
+                {
+                    user = tempUser;
+                }
+                else
+                {
+                    prac = tempUser;
+                }
 
+            }
+            message.To.Add(new MailboxAddress(user.Name, user.Email));
+
+            message.Subject = "Bekræftelses Email til " + user.Name;
+
+            message.Body = new TextPart("plain")
+            {
+                Text = @"Dette er en meddelelse om at " + user.Name + "'s aftale d. " + appointment.DateAndTime.ToString("dd/MM/yyyy HH:mm") + " med " + prac.Name + " er blevet aflyst." +                
+                "\n Vi beklager ulejligheden og håber at du vil komme til os igen på at andet tidspunkt" +
+                "\nHvis der er nogle problemer eller spørgsmål til din tid er du som altid velkommen til at kontakte os på mobil eller mail" +
+                "\n Venlig hilsen" +
+                "\n Semplito"
+
+            };
+
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+
+                client.Connect("smtp.sendgrid.net", 587, false);
+
+                client.Authenticate("apikey", "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");
+
+                client.Send(message);
+                client.Disconnect(true);
+            }
         }
 
     }
