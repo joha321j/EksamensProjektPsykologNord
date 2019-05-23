df755d47314f02b2503e6196516a190f13af2a96
Johannes Ehlers Nyholm Thomsen <joha321j@edu.eal.dk>
2019-05-23 09:48:13 +0200

    Update LandingPage.xaml.cs

--- Projekt/NoNameBooking/BookNyAftale/LandingPage.xaml.cs
+++ Projekt/NoNameBooking/BookNyAftale/LandingPage.xaml.cs
@@ -1,20 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using ApplicationClassLibrary;
-using System.Xml;
 
 namespace BookNyAftale
 {
@@ -283,8 +274,6 @@ namespace BookNyAftale
                     break;
                 case "lvSunday":
                     item = lvSunday.SelectedItems[0] as ListViewItem;
-                    break;
-                default:
                     break;
             }
             
